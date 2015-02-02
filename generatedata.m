27ed6b43273a78ee618f636fac1b6be2cb0ad875
Chris Cox <crcox@wisc.edu>
2015-02-02 08:33:32 -0600

    Begining of generate data function. Notion of groups can help titrate the differences among simulated subjects.

---
+++ generatedata.m
@@ -0,0 +1,53 @@
+function [X,G] = generatedata(n,d,varargin)
+  if n > d
+    defaultNActVox = d;
+  else
+    defaultNActVox = n;
+  end
+  p = inputParser;
+  addRequired(p,'n',@isnumeric);
+  addRequired(p,'d',@isnumeric);
+  addParameter(p,'groupsize',1,@isnumeric);
+  addParameter(p,'nactivevox',defaultNActVox,@isnumeric);
+  addParameter(p,'nactivegrp',1,@isnumeric);
+  addParameter(p,'distributed',true,@islogical);
+  addParameter(p,'snr',1,@isnumeric);
+  addParameter(p,'nsubjects',1,@isnumeric);
+  parse(p,n,d,varargin{:});
+
+  gsz = p.Results.groupsize;
+  nactivevox = p.Results.nactivevox;
+  nactivegrp = p.Results.nactivegrp;
+
+  if nactivevox > (nactivegrp * gsz)
+    if gsz == 1
+      nactivegrp = nactivevox;
+    else
+      error('Number of desired active voxels exceeds the total number voxels across desired active groups.')
+    end
+  end
+
+  X = cell(p.Results.nsubjects,1);
+  y = false(n,1);
+  y(1:floor(n/2)) = true;
+
+  r = mod(d,gsz);
+  g = [1:d,nan(1,r)];
+  G = reshape(g,(d+r)/gsz,gsz);
+  
+  if p.Results.distributed
+    actgrp = randperm(size(G,1),nactivegrp);
+  else
+    actgrp = 1:nactivegrp;
+  end
+
+  z = G(actgrp,:);
+  z = z(:);
+  
+  for i = 1:length(X)
+    X{i} = zeros(n,d);
+    activevox = z(randperm(length(z),nactivevox));
+    X{i}(y,activevox) = p.Results.snr;
+    X{i} = X{i} + randn(n,d);
+  end
+end
